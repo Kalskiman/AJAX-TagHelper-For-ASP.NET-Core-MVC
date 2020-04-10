0c90f46f4683178f01aa78481af84f8bcfa1d6dd
Navid Faridi <navid.faridi@gmail.com>
2020-04-10 13:01:08 +0300

    adding projects

---
+++ NF.Samples.AjaxTagHelper/NF.Samples.UsingAjaxTagHelpers/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NF.Samples.UsingAjaxTagHelpers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
